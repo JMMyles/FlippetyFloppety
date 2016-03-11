f468bab53f9e65d61a435ca2e1e1f1cc02dfd51e
jeanielam <jeanielam321@gmail.com>
2016-03-11 12:04:19 -0800

    final table changes

--- all.sql
+++ all.sql
@@ -20,62 +20,63 @@ drop table productinfo;
 drop table equipment;
 -- Create each table.
 create table supervisor(
-	ssid varchar2(10) primary key,
+	ssid varchar2(10) NOT NULL primary key,
 	sname varchar2(40)
 	);
 create table researcher(
-	rsid varchar2(10) primary key,
+	rsid varchar2(10) NOT NULL primary key,
 	rname varchar2(40)
 	);
 create table labbook(
-	booknum int primary key,
-	rsid varchar(10) NOT NULL,
+	booknum int NOT NULL primary key,
+	rsid varchar2(10) NOT NULL,
 	foreign key (rsid) references researcher
 	);
 create table experiment(
 	-- JL: i dont think this one is right in the google doc the on update cascade, on delete cascade
 	-- JM: I've added the other bits of the table anyway.  We'll have a look.
-	book# int,
+	booknum int,
 	cdate date,
-	page# int,
-	primary key (book#, cdate)
+	pagenum int,
+	primary key (booknum, cdate),
+	foreign key (booknum) references labbook on delete cascade
 	);
 create table inventory(
-	iid varchar(10) primary key,
-	iloc varchar(40),
-	datec date,
-	qnty integer,
-	iname varchar2(40)
+	iid varchar2(10) NOT NULL primary key,
+	iloc varchar2(40) NOT NULL,
+	datec date NOT NULL,
+	qnty integer NOT NULL,
+	iname varchar2(40) NOT NULL
 	);
 create table eusesi(
-	book# int NOT NULL,
+	booknum int NOT NULL,
 	cdate date NOT NULL,
-	iid varchar(10),
-	primary key book#, date, iid,
+	iid varchar2(10),
+	primary key booknum, date, iid,
 	foreign key cdate references inventory,
-	foreign key book# references labbook
+	foreign key booknum references labbook
 	);
 create table sreviewsi(
-	lastchecked date,
-	ssid varchar(10),
-	iid varchar(10),
+	lastchecked date NOT NULL,
+	ssid varchar2(10) NOT NULL,
+	iid varchar2(10) NOT NULL,
 	primary key (ssid, iid),
 	foreign key (ssid) references supervisor,
 	foreign key (iid) references inventory
 	);
 create table rupdatei(
-	rsid varchar(10),
-	iid varchar(10),
-	lastchecked date,
+	rsid varchar2(10) NOT NULL,
+	iid varchar2(10) NOT NULL,
+	lastchecked date NOT NULL,
 	primary key (rsid, iid),
 	foreign key (rsid) references researcher,
 	foreign key (iid) references inventory
 	);
 create table labcreated(
-	iid varchar(10) primary key,
-	amnt double,
-	booknum integer,
-	datelc date,
+	iid varchar2(10) primary key,
+	amnt double NOT NULL,
+	booknum integer NOT NULL,
+	datelc date NOT NULL,
 	foreign key (iid) references inventory,
 	foreign key (booknum) references labbook,
 	foreign key (datelc) references experiment
@@ -84,46 +85,44 @@ create table labcreated(
 -- Does references labcreated make sense? Can we cascade up to inventory to update that table?
 -- Or is there some other way we should inforce this restriction?
 	create table rcreatesi(
-	rsid varchar(10),
-	iid varchar(10),
-	iname varchar2(40),
-	-- does iname really need to be a part of the key?
+	rsid varchar2(10) NOT NULL,
+	iid varchar2(10) NOT NULL,
 	primary key (rsid, iid, iname),
 	foreign key (rsid) references researcher,
-	foreign key (iid, iname) references labcreated
+	foreign key (iid) references labcreated
 	);
 create table machinery(
-	iid varchar(10) primary key,
-	serialnum varchar(10),
+	iid varchar2(10) primary key,
+	serialnum varchar2(10),
 	foreign key (iid) references equipment
 	);
 create table consumable(
-	iid varchar(10) primary key,
-	amnt float,
+	iid varchar2(10) primary key,
+	amnt float NOT NULL,
 	foreign key (iid) references equipment
 	);
 create table inspection(
-	datec date,
-	iid varchar(10),
+	datec date NOT NULL,
+	iid varchar2(10) NOT NULL,
 	primary key(datec, iid),
 	foreign key (iid) references machinery
 	);
 create table breakdown(
-	iid varchar(10),
-	datec date,
-	description varchar(40),
+	iid varchar2(10) NOT NULL,
+	datec date NOT NULL,
+	description varchar2(40) NOT NULL,
 	primary key(iid, datec),
 	foreign key (iid) references machinery
 	);
 create table productinfo(
-	supplier varchar2(40),
-	ordernum varchar(10),
+	supplier varchar2(40) NOT NULL,
+	ordernum varchar2(10) NOT NULL,
 	primary key (supplier, ordernum)
 	);
 create table equipment(
-	iid varchar(10) primary key,
-	supplier varchar2(40),
-	ordernum varchar(10),
+	iid varchar2(10) primary key,
+	supplier varchar2(40) NOT NULL,
+	ordernum varchar2(10) NOT NULL,
 	foreign key (inventory) references inventory,
-	foreign key (ordernum) references productinfo
+	foreign key (ordernum, supplier) references productinfo,
 	);
