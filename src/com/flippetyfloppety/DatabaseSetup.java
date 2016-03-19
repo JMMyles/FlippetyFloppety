a589e9c5ab6610761d28f56ca5656e4b706b8003
jeanielam <jeanielam321@gmail.com>
2016-03-18 17:53:10 -0700

    base of intellij

---
+++ src/com/flippetyfloppety/DatabaseSetup.java
@@ -0,0 +1,29 @@
+package com.flippetyfloppety;
+import java.sql.*;
+
+public class DatabaseSetup {
+    private Connection connection = null;
+
+    public DatabaseSetup() {
+        try {
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+            String url = "jdbc:oracle:thin:@localhost:1522:ug";
+            this.connection = DriverManager.getConnection(url, "ora_q2d9", "a32916132");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public Connection getConnection(){
+        return this.connection;
+    }
+
+    public boolean closeConnection() {
+        try {
+            this.connection.close();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
