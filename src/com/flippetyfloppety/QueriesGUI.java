5a40ffc46bdcb03ef0dc02f315df498dacd5e429
JMMyles <JosephMMyles@gmail.com>
2016-03-25 11:42:53 -0700

    Initial QueriesGUI.java.  Nowhere near working.

---
+++ src/com/flippetyfloppety/QueriesGUI.java
@@ -0,0 +1,16 @@
+package com.flippetyfloppety;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+import java.awt.*;
+import java.awt.TextField;
+
+public class QueriesGUI {
+
+    public void projectionSelectionQuery() {
+        TextField tf1;
+        // projection and selection
